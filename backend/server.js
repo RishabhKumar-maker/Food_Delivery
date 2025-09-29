47639fff3503e03910f53e73e1baaf2bdf806623
Rishabh Kumar <2022uee1098@mnit.ac.in>
2025-09-29 21:21:58 +0530

    rate limiter

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,21 @@ import userRouter from './routes/userRoute.js';
 import 'dotenv/config'
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 const port = process.env.PORT;
 
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per 15 minutes
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+  statusCode: 429, // Optional: default is 429
+  headers: true, // Send X-RateLimit-Limit, X-RateLimit-Remaining, and Retry-After headers
+});
+
 app.use(cors());
+app.use(apiLimiter); // Apply rate limiting to all requests
 app.use(express.json());
 app.use('/images', express.static('uploads')); // Serve static files from the 'uploads' directory
 
